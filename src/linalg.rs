704ecb601e9c5511d0299cd17237f3b104071b96
Isak Jägberg <ijagberg@gmail.com>
2021-02-20 22:51:32 +0100

    Add a bunch of matrix ops

--- src/linalg.rs
+++ src/linalg.rs
@@ -1,13 +1,69 @@
-use std::ops::{Add, Mul, Sub};
-
-use num_traits::PrimInt;
-
 use crate::Grid;
+use num_traits::Num;
+use std::ops::{Add, Mul, Sub};
 
 impl<T> Grid<T>
 where
-    T: PrimInt,
+    T: Num + Copy,
 {
+    pub fn identity(size: usize) -> Self {
+        if size == 0 {
+            panic!();
+        }
+
+        let mut data = Vec::new();
+
+        for _ in 0..size - 1 {
+            data.push(T::one());
+            for _ in 0..size {
+                data.push(T::zero());
+            }
+        }
+        data.push(T::one());
+
+        Self::new(size, size, data)
+    }
+
+    pub fn is_symmetric(&self) -> bool {
+        self == &self.transpose()
+    }
+
+    pub fn trace_checked(&self) -> Option<T> {
+        if self.is_empty() || !self.is_square() {
+            None
+        } else {
+            Some(self._trace())
+        }
+    }
+
+    pub fn trace(&self) -> T {
+        if self.is_empty() || !self.is_square() {
+            panic!()
+        } else {
+            self._trace()
+        }
+    }
+
+    fn _trace(&self) -> T {
+        let mut sum = T::zero();
+        for n in 0..self.width() {
+            sum = sum + self[(n, n)];
+        }
+
+        sum
+    }
+
+    /// Calculate the determinant of a square `Grid`.
+    ///
+    /// # Example
+    /// ```rust
+    /// # use simple_grid::Grid;
+    /// let two_by_two = Grid::new(2, 2, vec![3, 8, 4, 6]);
+    /// assert_eq!(two_by_two.determinant(), -14);
+    /// ```
+    /// # Panics
+    /// * If `self` is an empty grid.
+    /// * If `self` is not a square grid.
     pub fn determinant(&self) -> T {
         if self.is_empty() {
             panic!("cannot calculate the determinant of an empty matrix");
@@ -36,6 +92,42 @@ where
         sum.unwrap()
     }
 
+    pub fn is_upper_triangular(&self) -> bool {
+        if self.is_empty() || !self.is_square() {
+            return false;
+        }
+
+        for row in 0..self.height() {
+            for column in 0..row {
+                if self[(column, row)] != T::zero() {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+
+    pub fn is_lower_triangular(&self) -> bool {
+        if self.is_empty() || !self.is_square() {
+            return false;
+        }
+
+        for column in 0..self.width() {
+            for row in 0..column {
+                if self[(column, row)] != T::zero() {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+
+    pub fn is_triangular(&self) -> bool {
+        self.is_upper_triangular() && self.is_lower_triangular()
+    }
+
     fn minor(&self, skip_column: usize, skip_row: usize) -> Grid<T> {
         let mut new_vec: Vec<T> = Vec::with_capacity((self.width() - 1) * (self.height() - 1));
 
@@ -59,8 +151,11 @@ where
 
     fn mul(self, rhs: Grid<T>) -> Self::Output {
         if self.width() != rhs.height() {
-            panic!("invalid matrix dimensions for multiplication, lhs: {} columns, {} rows, rhs: {} columns, {} rows", self.width(), 
-            self.height(), rhs.width(), rhs.height());
+            panic!(
+                "invalid matrix dimensions for multiplication, lhs: {} columns, rhs: {} rows",
+                self.width(),
+                rhs.height()
+            );
         }
 
         let mut product_vec: Vec<T> = Vec::with_capacity(self.height() * rhs.width());
@@ -110,8 +205,11 @@ where
 
     fn add(self, rhs: Grid<T>) -> Self::Output {
         if self.width() != rhs.width() || self.height() != rhs.height() {
-            panic!("invalid matrix dimensions for addition, lhs: {} columns, {} rows, rhs: {} columns, {} rows", self.width(), 
-            self.height(), rhs.width(), rhs.height()    );
+            panic!("invalid matrix dimensions for addition, lhs: {} columns, {} rows, rhs: {} columns, {} rows", 
+                self.width(),
+                self.height(),
+                rhs.width(),
+                rhs.height());
         }
 
         let mut sum_vec = Vec::with_capacity(self.area());
@@ -134,8 +232,11 @@ where
 
     fn sub(self, rhs: Grid<T>) -> Self::Output {
         if self.width() != rhs.width() || self.height() != rhs.height() {
-            panic!("invalid matrix dimensions for addition, lhs: {} columns, {} rows, rhs: {} columns, {} rows", self.width(), 
-            self.height(), rhs.width(), rhs.height()    );
+            panic!("invalid matrix dimensions for addition, lhs: {} columns, {} rows, rhs: {} columns, {} rows",
+                self.width(),
+                self.height(),
+                rhs.width(),
+                rhs.height());
         }
 
         let mut sum_vec = Vec::with_capacity(self.area());
@@ -162,6 +263,37 @@ mod tests {
         let product = a * b;
 
         assert_eq!(product, Grid::new(2, 2, vec![58, 64, 139, 154]));
+
+        let a = Grid::new(1, 4, vec![0.0, 0.5, -2.1, 1.0001]);
+        let b = Grid::new(4, 1, vec![0.005, 9.7, -10.1, 0.0]);
+
+        let product = a * b;
+
+        assert_eq!(
+            product,
+            Grid::new(
+                4,
+                4,
+                vec![
+                    0.0,
+                    0.0,
+                    -0.0,
+                    0.0,
+                    0.0025,
+                    4.85,
+                    -5.05,
+                    0.0,
+                    -0.0105,
+                    -20.37,
+                    21.21,
+                    -0.0,
+                    0.0050005,
+                    9.70097,
+                    -10.101009999999999,
+                    0.0
+                ]
+            )
+        );
     }
 
     #[test]
@@ -195,12 +327,64 @@ mod tests {
 
     #[test]
     fn determinant_test() {
-        let a = Grid::new(2, 2, vec![3, 8, 4, 6]);
+        let two_by_two = Grid::new(2, 2, vec![3, 8, 4, 6]);
 
-        assert_eq!(a.determinant(), -14);
+        assert_eq!(two_by_two.determinant(), -14);
 
-        let b = Grid::new(3, 3, vec![6, 1, 1, 4, -2, 5, 2, 8, 7]);
+        let three_by_three = Grid::new(3, 3, vec![6, 1, 1, 4, -2, 5, 2, 8, 7]);
 
-        assert_eq!(b.determinant(), -306);
+        assert_eq!(three_by_three.determinant(), -306);
+
+        let five_by_five = Grid::new(
+            5,
+            5,
+            vec![
+                0, 6, -2, -1, 5, 0, 0, 0, -9, -7, 0, 15, 35, 0, 0, 0, -1, -11, -2, 1, -2, -2, 3, 0,
+                -2,
+            ],
+        );
+
+        assert_eq!(five_by_five.determinant(), 2480);
+    }
+
+    #[test]
+    fn determinant_float_test() {
+        let g = Grid::new(
+            3,
+            3,
+            vec![1.0, 2.5, -9.0, 3.7, 2.1, -1.11, 12.3, -81.17, -10.0],
+        );
+
+        assert_eq!(g.determinant(), 2882.6998);
+    }
+
+    #[test]
+    fn is_triangular_test() {
+        let g = Grid::new(3, 3, vec![1, 4, 1, 0, 6, 4, 0, 0, 1]);
+        assert!(g.is_upper_triangular());
+
+        let g = Grid::new(3, 3, vec![1, 0, 0, 2, 8, 0, 4, 9, 7]);
+        assert!(g.is_lower_triangular());
+
+        let g = Grid::new(3, 3, vec![1, 0, 0, 0, 2, 0, 0, 0, 3]);
+        assert!(g.is_triangular());
+    }
+
+    #[test]
+    fn identity_test() {
+        let g = Grid::identity(1);
+        assert_eq!(g, Grid::new(1, 1, vec![1]));
+
+        let g = Grid::identity(4);
+        assert_eq!(
+            g,
+            Grid::new(4, 4, vec![1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])
+        );
+    }
+
+    #[test]
+    fn is_symmetric_test() {
+        let g = Grid::new(3, 3, vec![1, 7, 3, 7, 4, 5, 3, 5, 6]);
+        assert!(g.is_symmetric());
     }
 }
